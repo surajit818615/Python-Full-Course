31d5cc21fa356900001f957fed9bd154d3bcb1de
Surajit Mandal <132629241+surajit818615@users.noreply.github.com>
2024-07-11 06:44:35 +0530

    Add files via upload

---
+++ String.py
@@ -0,0 +1,20 @@
+name = 'Surajit'
+print("Hello," + name)
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+print(name[5])
+print(name[6])
+
+a ="""This is my new car. 
+It is my dream car which I want from my adultness.
+My parents are also like this car.
+This car name in Lamborgini."""
+print(a)
+for character in a:
+    print(character)
+
+
+
---
+++ String_slicing.py
@@ -0,0 +1,11 @@
+fruit = "Mango"
+len1 = len(fruit)
+print("Mango is a ",len1, "Letter word")
+name = "Surajit"
+len2 = len(name)
+print("Surajit is a ",len2 ,"Word letter. Thala for the reason")
+pie = "Applepie"
+print(pie[:5]) #slicing from start
+print(pie[5:]) #slicing till end
+print(pie[2:6]) #slicing in between
+print(pie[-8:]) #slicing using negetive index
